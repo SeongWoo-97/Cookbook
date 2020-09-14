1986eb0e5172614136219d700cd4f73a2506c4b7
SeongWoo97 <jsw9058@gmail.com>
2020-09-15 00:56:31 +0900

    Navigation todo app

---
+++ navigation_todo_app/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      // TodoScreen 에 List 를 만들어 전달
+      home: TodoScreen(
+        todos: List.generate(
+            20,
+            (index) => Todo('Todo $index',
+                'A description of what needs to be done for Todo $index')),
+      ),
+    );
+  }
+}
+
+class TodoScreen extends StatelessWidget {
+  final List<Todo> todos;
+
+  TodoScreen({Key key, @required this.todos}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todos'),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            onTap: () {
+              Navigator.push(
+                context, MaterialPageRoute(builder: (context) => DetailScreen(todo: todos[index],))
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final Todo todo;
+
+  DetailScreen({Key key, @required this.todo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(todo.description),
+      ),
+    );
+  }
+}
+
+class Todo {
+  final title;
+  final description;
+
+  Todo(this.title, this.description);
+}
